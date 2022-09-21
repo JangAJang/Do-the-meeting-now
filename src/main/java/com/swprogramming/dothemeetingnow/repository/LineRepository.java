a355944744cc27f653da4e7f767f7ff9fb7d78b1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-09-21 09:21:55 +0900

    feat: add LineService

--- src/main/java/com/swprogramming/dothemeetingnow/repository/LineRepository.java
+++ src/main/java/com/swprogramming/dothemeetingnow/repository/LineRepository.java
@@ -3,6 +3,7 @@ package com.swprogramming.dothemeetingnow.repository;
 import com.swprogramming.dothemeetingnow.entity.Line;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
