3f02650f7f0f4693ef9e8b5af62f4aa0dfe56183
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-09-29 17:07:36 +0900

    feat: add Route Controller

--- src/main/java/com/swprogramming/dothemeetingnow/controller/RouteController.java
+++ src/main/java/com/swprogramming/dothemeetingnow/controller/RouteController.java
@@ -1,15 +1,39 @@
 package com.swprogramming.dothemeetingnow.controller;
 
+import com.swprogramming.dothemeetingnow.dto.route.AddRouteRequestDto;
+import com.swprogramming.dothemeetingnow.response.Response;
+import com.swprogramming.dothemeetingnow.service.RouteService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/route")
 public class RouteController {
 
+    private final RouteService routeService;
+
     @ApiOperation(value = "", notes = "")
-    
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/")
+    public Response addRoute(@RequestBody AddRouteRequestDto addRouteRequestDto){
+        return Response.success(routeService.addRoute(addRouteRequestDto));
+    }
+
+    @ApiOperation(value = "", notes = "")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public Response updateRoute(@RequestBody AddRouteRequestDto addRouteRequestDto, @RequestParam("${id}") Long id){
+        return Response.success(routeService.updateRoute(addRouteRequestDto, id));
+    }
+
+    @ApiOperation(value = "", notes = "")
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{id}")
+    public Response deleteRoute(@RequestParam("${id}")Long id){
+        return Response.success(routeService.deleteRoute(id));
+    }
+
 }
