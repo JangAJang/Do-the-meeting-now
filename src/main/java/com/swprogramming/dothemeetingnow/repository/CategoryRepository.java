bf56906652fe8067193347d93e965899c524f163
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-22 21:16:12 +0900

    refactor : add Optional<> on findByName

--- src/main/java/com/swprogramming/dothemeetingnow/repository/CategoryRepository.java
+++ src/main/java/com/swprogramming/dothemeetingnow/repository/CategoryRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Category findByName(String name);
+    Optional<Category> findByName(String name);
 }
